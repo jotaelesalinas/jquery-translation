de230d8017327e9ab7782cb810defa9e330fa1bc
José Luis Salinas Jiménez <qwertz@example.com>
2012-08-30 17:57:42 +0200

    added events

--- jquery.translate.js
+++ jquery.translate.js
@@ -38,72 +38,6 @@
 			return trans_table[settings.lang][key];
 		};
 		
-		// interaction with HTML DOM
-		
-		var extract_strids = function () {
-			plugin.each( function () {
-				var strid = $(this).data(DATA_STR_ID).trim();
-				if ( strid !== '' ) {
-					if ( (typeof get_trans(strid) === 'undefined') ||
-					     (get_trans(strid) === null) ||
-					     (get_trans(strid) == strid) ) {
-						set_trans(strid, false);
-					}
-				}
-			} );
-		};
-		
-		var do_translation = function () {
-			plugin.each( function () {
-				// XXX check data-strid
-				var strid = $(this).data(DATA_STR_ID).trim();
-				if ( strid === '' ) {
-					return;
-				}
-				
-				var text = get_trans(strid);
-				
-				if (text === false) {
-					text = strid;
-					console.log("Warning: missing translation string '" + strid + "' (language: " + settings.lang + ")");
-					if (settings.callback_trans_not_found) {
-						settings.callback_trans_not_found($(this)[0], settings.lang, strid);
-					} else {
-						$(this).addClass(CLASS_NO_TRANS);
-					}
-				} else {
-					var re = /\[_(\w+)_\]/g;
-					var params = [],
-						full_params = [];
-					while (match = re.exec(text)) {
-						full_params.push(match[0]);
-						params.push(match[1]);
-					}
-					
-					for (var i = 0; i < params.length; i++) {
-						var p = params[i],
-							full_p = full_params[i];
-						
-						var key = DATA_PARAM_PREFIX + p.toLowerCase();
-						var value = $(this).data(key);
-						if ( (typeof value === 'undefined') || (value === null) ) {
-							value = full_p;
-							console.log("Warning: missing parameter '" + full_p + "' in translation string '" + strid + "' (language: " + settings.lang + ")");
-							if (settings.callback_missing_param) {
-								settings.callback_missing_param($(this)[0], settings.lang, strid, full_p);
-							} else {
-								$(this).addClass(CLASS_NO_PARAM);
-							}
-						}
-						
-						text = text.replace(full_p, value);
-					}
-				}
-				
-				$(this).html( text );
-			} );
-		};
-		
 		// a pair of polyfills
 		
 		// trim
@@ -132,11 +66,63 @@
 		
 		var plugin = this;
 		
-		extract_strids();
-		do_translation();
+		plugin.each( function () {
+			var strid = $(this).data(DATA_STR_ID).trim();
+			if ( strid !== '' ) {
+				if ( (typeof get_trans(strid) === 'undefined') ||
+					 (get_trans(strid) === null) ||
+					 (get_trans(strid) == strid) ) {
+					set_trans(strid, false);
+				}
+			}
+			
+			// XXX check data-strid
+			var strid = $(this).data(DATA_STR_ID).trim();
+			if ( strid === '' ) {
+				return;
+			}
+			
+			var text = get_trans(strid);
+			
+			if (text === false) {
+				text = strid;
+				// XXX check setting for this
+				// console.log("Warning: missing translation string '" + strid + "' (language: " + settings.lang + ")");
+				// $(this).addClass(CLASS_NO_TRANS);
+				$(this).trigger('fail_strid.translate', [$($(this)[0]), settings.lang, strid]);
+			} else {
+				var re = /\[_(\w+)_\]/g;
+				var params = [],
+					full_params = [];
+				while (match = re.exec(text)) {
+					full_params.push(match[0]);
+					params.push(match[1]);
+				}
+				
+				for (var i = 0; i < params.length; i++) {
+					var p = params[i],
+						full_p = full_params[i];
+					
+					var key = DATA_PARAM_PREFIX + p.toLowerCase();
+					var value = $(this).data(key);
+					if ( (typeof value === 'undefined') || (value === null) ) {
+						value = full_p;
+						// XXX check setting for this
+						// console.log("Warning: missing parameter '" + full_p + "' in translation string '" + strid + "' (language: " + settings.lang + ")");
+						// $(this).addClass(CLASS_NO_PARAM);
+						$(this).trigger('fail_param.translate', [$($(this)[0]), settings.lang, strid, full_p]);
+					}
+					
+					text = text.replace(full_p, value);
+				}
+			}
+			
+			$(this).html( text );
+		} );
+		
+		$(plugin.selector).triggerHandler('finished.translate', [plugin.selector]);
 		
 		return plugin.each( function () {} );
-		
 	};
 	
 	$.fn.translate.defaults = {
